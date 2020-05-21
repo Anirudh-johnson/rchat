4942c46e8d1f97291cd160e2eb766222d0ea9895
Anirudh <Anirudh-johnson@users.noreply.github.com>
2020-05-21 20:47:40 +0530

    Final commit to github after psswrd hashing '

--- wtforms_fields.py
+++ wtforms_fields.py
@@ -25,13 +25,16 @@ class RegistrationForm(FlaskForm):
 
     username = StringField('username_label',
         validators=[InputRequired(message='username required'),
-        Length(min=4, max=25, message="username must be between 4 and 25 characters")])
+                    Length(min=4, max=25, message="username must be between 4 and 25 characters")])
+
     password = PasswordField('password_label',
         validators=[InputRequired(message='password required'),
-        Length(min=4, max=25, message="password must be between  4 and 25 characters")])
+                    Length(min=4, max=25, message="password must be between  4 and 25 characters")])
+
     confirm_pswd = PasswordField('confirm_pswd_label',
         validators=[InputRequired(message='password required'),
-        EqualTo('password', message="Password must match")])
+                    EqualTo('password', message="Password must match")])
+
     submit_button = SubmitField('Create')
 
 
